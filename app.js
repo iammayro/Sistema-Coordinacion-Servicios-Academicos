ae6b205dc83a42a57240be46163e937594a9946a
launderex619 <carlos12150@hotmail.com>
2020-04-10 20:01:57 -0500

    Agregado prettier (herramienta para escribir codigo correctamente)

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 // Cargar las variables de entorno
 require('dotenv').config();
 
 // Conectar a mongoose
-mongoose.connect('mongodb://localhost:27017/CSA', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+mongoose.connect('mongodb://localhost:27017/CSA', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-    console.log(`[status] Conectado a Mongoose`);
+  console.log(`[status] Conectado a Mongoose`);
 });
 
 // Body parser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 
 // Rutas divididas en modulos
@@ -25,5 +35,5 @@ app.use('/pro', require('./Routes/prodep.routes'));
 
 // Correr el puerto
 app.listen(process.env.PORT, function() {
-    console.log(`[status] API Corriendo en el puerto ${process.env.PORT}`);
+  console.log(`[status] API Corriendo en el puerto ${process.env.PORT}`);
 });
