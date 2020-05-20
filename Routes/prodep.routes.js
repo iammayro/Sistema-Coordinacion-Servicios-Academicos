87817dbf7768a434c3c2c11e866cfc781bea120b
Iszael Gonzalez <iszael.gonzalez@alumnos.udg.mx>
2020-05-20 00:37:07 -0500

    se añadio verficaciones a rutas

--- Routes/prodep.routes.js
+++ Routes/prodep.routes.js
@@ -5,39 +5,67 @@ const app = express();
 const prodepCtrl = require('../Controllers/prodep.controller');
 const prodepExpedienteCtrl = require('../Controllers/prodep_expediente.controller');
 const prodepDictamenCtrl = require('../Controllers/prodep_dictamen.controller');
+const authCtrl = require('../Controllers/auth.controller');
 const archivos = require('../Middlewares/multer').uploadFile;
+app.use(authCtrl.validarCredenciales);
 
-app.post('/:codigo', prodepCtrl.crearProdep);
-app.put('/:codigo/actualizar', prodepCtrl.actualizarProdep);
-app.get('/:codigo', prodepCtrl.verProdep);
+app.post(
+  '/:codigo',
+  authCtrl.limitarAcceso('coordinador de personal'),
+  prodepCtrl.crearProdep
+);
+app.put(
+  '/:codigo/actualizar',
+  authCtrl.limitarAcceso('coordinador de personal'),
+  prodepCtrl.actualizarProdep
+);
+app.get(
+  '/:codigo',
+  prodepCtrl.verProdep
+);
 
 app.post(
   '/:codigo/expediente',
+  authCtrl.limitarAcceso('coordinador de personal'),
   archivos.single('expediente'),
   prodepExpedienteCtrl.subirExpediente
 );
 app.put(
   '/:codigo/expediente/actualizar',
+  authCtrl.limitarAcceso('coordinador de personal'),
   archivos.single('expediente'),
   prodepExpedienteCtrl.actualizarExpediente
 );
 app.get(
   '/:codigo/expediente/descargar',
+  authCtrl.limitarAcceso('coordinador de personal', 'maestro'),
   prodepExpedienteCtrl.descargarExpediente
 );
-app.get('/:codigo/expediente', prodepExpedienteCtrl.verExpediente);
+app.get(
+  '/:codigo/expediente',
+  prodepExpedienteCtrl.verExpediente
+);
 
 app.post(
   '/:codigo/dictamen',
+  authCtrl.limitarAcceso('coordinador de personal'),
   archivos.single('dictamen'),
   prodepDictamenCtrl.subirDictamen
 );
 app.put(
   '/:codigo/dictamen/actualizar',
+  authCtrl.limitarAcceso('coordinador de personal'),
   archivos.single('dictamen'),
   prodepDictamenCtrl.actualizarDictamen
 );
-app.get('/:codigo/dictamen/descargar', prodepDictamenCtrl.descargarDictamen);
-app.get('/:codigo/dictamen', prodepDictamenCtrl.verDictamen);
+app.get(
+  '/:codigo/dictamen/descargar',
+  authCtrl.limitarAcceso('coordinador de personal', 'maestro'),
+  prodepDictamenCtrl.descargarDictamen
+);
+app.get(
+  '/:codigo/dictamen',
+  prodepDictamenCtrl.verDictamen
+);
 
 module.exports = app;
